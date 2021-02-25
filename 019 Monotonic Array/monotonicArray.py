a8f3eba1f388bee44917863e138c0cc9ce80659c
BrianAKass <kasinator009@gmail.com>
2021-02-25 14:10:05 -0500

    python added

---
+++ 019 Monotonic Array/monotonicArray.py
@@ -0,0 +1,9 @@
+def isMonotonic(array):
+    isNonDecreasing = True
+    isNonIncreasing = True
+    for i in range(1, len(array)):
+        if array[i] < array[i-1]:
+            isNonDecreasing = False
+        if array[i] > array[i-1]:
+            isNonIncreasing = False
+    return isNonIncreasing or isNonDecreasing
