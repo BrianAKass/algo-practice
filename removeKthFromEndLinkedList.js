1b5784455a375817267a7e8fed5f4dbc297e135a
Brian Kass <kasinator009@gmail.com>
2022-01-10 09:43:40 -0500

    more algos

---
+++ removeKthFromEndLinkedList.js
@@ -0,0 +1,26 @@
+class LinkedList {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+// O(n) time | O(1) space
+function removeKthNodeFromEnd(head, k) {
+  let counter = 1;
+	let first = head;
+	let second = head;
+	while (counter <= k) {
+		second = second.next;
+		counter++;
+	}
+	if (second === null) {
+		head.value = head.next.value;
+		head.next = head.next.next;
+		return;
+	}
+	while (second.next !== null) {
+		second = second.next;
+		first = first.next;
+	}
+	first.next = first.next.next;
+}
